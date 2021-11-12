cf59df5a52c9d59c54fff2f68838c25ed39a81d8
sonanisatharva <sonanisatharva98@gmail.com>
2021-11-12 17:22:52 -0500

    Day 2 Problem 1

---
+++ day_2/day2_prob1.py
@@ -0,0 +1,19 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+n = [str(line.strip()) for line in lines]
+# print(n)
+
+answer = 0
+for i in n:
+    first, second = i.split(':')
+    f,letter = first.split()
+    num1, num2 = f.split('-')
+    count = 0
+    for i in second:
+        if (i == letter):
+            count += 1
+    if (int(num1) <= count <= int(num2)):
+        answer += 1
+
+print('ANSWER = ', answer)
