06bd3d02e882f36459b1df06e34d5d8cae39964d
sonanisatharva <sonanisatharva98@gmail.com>
2021-11-17 00:29:13 -0500

    Day 5 Problem 1

--- day_5/day5_prob1.py
+++ day_5/day5_prob1.py
@@ -1,14 +1,43 @@
 with open('input.txt') as f:
     lines = f.readlines()
 input = [(line.strip('\n')) for line in lines]
-print(input)
+# print(input)
 
-def f_letter():
+def row(seat):
+    l , h = 0 , 127
+    for i in range(6):
+        m = (l+h)//2
+        if seat[i] == 'F':
+            h = m
+        elif seat[i] == 'B':
+            l = m + 1
+    if seat[6] == 'F':
+        return l
+    else:
+        return h
 
-def seat_id(i):
-    return i * 8 + 5
+def column(seat):
+    l , h = 0 , 7
+    for i in range(2):
+        m = (l+h)//2
+        if seat[i] == 'L':
+            h = m
+        elif seat[i] == 'R':
+            l = m + 1
+    if seat[6] == 'L':
+        return l
+    else:
+        return h
 
-h_seat = -1
+largest = -1
+seat_id = []
+for i in input:
+    r = row(i)
+    c = column(i)
+    id = r * 8 + c
+    seat_id.append(id)
+    if id > largest:
+        largest = id
 
-for seat in input:
-    row, column = seat[0:7] , seat[7:]
+print(largest)
+print(seat_id)
